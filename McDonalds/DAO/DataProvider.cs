ab7b2108fdf899269d06f12cc8c7a97a7f03640d
SevenScorpions <huyhoathangbmt@gmail.com>
2022-10-19 23:06:48 +0700

    Create DataProvider.cs

---
+++ McDonalds/DAO/DataProvider.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace McDonalds.DAO
+{
+    public class DataProvider
+    {
+        private static DataProvider instance;
+
+        public static DataProvider Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new DataProvider();
+                return instance;
+            }
+            private set => instance = value;
+        }
+        public DataTable ExcuteQuery(string query)
+        {
+
+            string connectionStr = @"Data Source=.\sqlexpress;Initial Catalog=MCDONALDS;Integrated Security=True";
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+
+
+    }
+}
