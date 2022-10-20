5eb6b9602570bd9649493af5d4769154407ce5a4
SevenScorpions <huyhoathangbmt@gmail.com>
2022-10-21 01:22:48 +0700

    update

--- McDonalds/DAO/TaiKhoanDAO.cs
+++ McDonalds/DAO/TaiKhoanDAO.cs
@@ -8,6 +8,16 @@ namespace McDonalds.DAO
 {
     public class TaiKhoanDAO
     {
-
+        private static TaiKhoanDAO instance;
+        public static TaiKhoanDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new TaiKhoanDAO();
+                return instance;
+            }
+            private set => instance = value;
+        }
     }
 }
