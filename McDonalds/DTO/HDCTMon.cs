fa7900b79df713f4d7e8c825e1bed448d4cd95b1
MaiHoaHT <89694382+MaiHoaHT@users.noreply.github.com>
2022-10-20 22:31:17 +0700

    DTO

---
+++ McDonalds/DTO/HDCTMon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McDonalds.DTO
+{
+    public class HDCTMon
+    {
+        private string idHD;
+        private string idCTMon;
+        private int soLuong;
+
+        public string IDHD
+        {
+            get { return idHD; }
+            set { idHD = value; }
+        }
+        public string IDCTMon
+        {
+            get { return idCTMon; }
+            set { idCTMon = value; }
+        }
+        public int SoLuong
+        {
+            get { return soLuong; }
+            set { soLuong = value; }
+        }
+        public HDCTMon(string idHD, string idCTMon, int soLuong)
+        {
+            IDHD = idHD;
+            idCTMon = idCTMon;
+            SoLuong = soLuong;
+        }
+        public HDCTMon(DataRow row)
+        {
+            IDHD = row["IDHD"].ToString();
+            IDCTMon = row["IDCTMON"].ToString();
+            SoLuong = (int)row["SOLUONG"];
+        }
+    }
+}
---
+++ McDonalds/DTO/HDMon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McDonalds.DTO
+{
+    public class HDMon
+    {
+        private string idHD;
+        private string idMon;
+        private int soLuong;
+
+        public string IDHD
+        {
+            get { return idHD; }
+            set { idHD = value; }
+        }
+        public string IDMon
+        {
+            get { return idMon; }
+            set { idMon = value; }
+        }
+        public int SoLuong
+        {
+            get { return soLuong; }
+            set { soLuong = value; }
+        }
+        public HDMon(string idHD, string idMon, int soLuong)
+        {
+            IDHD = idHD;
+            IDMon = idMon;
+            SoLuong = soLuong;
+        }
+        public HDMon(DataRow row)
+        {
+            IDHD = row["IDHD"].ToString();
+            IDMon = row["IDMON"].ToString();
+            SoLuong = (int)row["SOLUONG"];
+        }
+    }
+}
