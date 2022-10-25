bb3641dda6279ccac2c94b71f0aa28524cf97d8b
SevenScorpions <huyhoathangbmt@gmail.com>
2022-10-25 20:21:49 +0700

    Tuan 3

---
+++ McDonalds/DAO/VoucherDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Hoa
+namespace McDonalds.DAO
+{
+    internal class VoucherDAO
+    {
+    }
+}
