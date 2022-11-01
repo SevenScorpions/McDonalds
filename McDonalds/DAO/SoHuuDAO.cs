db0f06bbc59b35f330f9e0789c2a4a4e601c7349
MaiHoaHT <89694382+MaiHoaHT@users.noreply.github.com>
2022-11-02 01:35:01 +0700

    DAO-HOA
    DAO-HOA

--- McDonalds/DAO/SoHuuDAO.cs
+++ McDonalds/DAO/SoHuuDAO.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using McDonalds.DTO;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 //Hoa
 namespace McDonalds.DAO
 {
-    internal class SoHuuDAO
+    public class SoHuuDAO
     {
+        private static SoHuuDAO instance;
+        public static SoHuuDAO Instance
+        {
+            get { 
+                if(instance == null)
+                {
+                    instance = new SoHuuDAO();
+                }
+                return instance; 
+            }
+            private set => instance = value;
+        }
+        private SoHuuDAO() { }
+        public List<SoHuu> getSoHuu()
+        {
+            List<SoHuu> list = new List<SoHuu>();
+            string query = @"Select * from SOHUU";
+            DataTable data = DataProvider.Instance.ExcuteQuery(query);
+            foreach(DataRow dataRow in data.Rows)
+            {
+                list.Add(new SoHuu(dataRow));
+            }
+            return list;
+        }
+        public List<SoHuu> getSoHuu(string idKH)
+        {
+            List<SoHuu> list = new List<SoHuu>();
+            string query = @"Select * from SOHUU where IDKH = '" + idKH + "'";
+            DataTable data = DataProvider.Instance.ExcuteQuery(query);
+            foreach (DataRow dataRow in data.Rows)
+            {
+                list.Add(new SoHuu(dataRow));
+            }
+            return list;
+        }
+        public List<SoHuu> getSoHuu(string idKH, string idVoucher)
+        {
+            List<SoHuu> list = new List<SoHuu>();
+            string query = @"Select * from SOHUU where IDKH = '" + idKH + "' and IDVOUCHER = '" + idVoucher + "'";
+            DataTable data = DataProvider.Instance.ExcuteQuery(query);
+            foreach (DataRow dataRow in data.Rows)
+            {
+                list.Add(new SoHuu(dataRow));
+            }
+            return list;
+        }
+
+
     }
 }
--- McDonalds/DAO/VoucherDAO.cs
+++ McDonalds/DAO/VoucherDAO.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using McDonalds.DTO;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 //Hoa
 namespace McDonalds.DAO
 {
-    internal class VoucherDAO
+    public class VoucherDAO
     {
+        private static VoucherDAO instance;
+        public static VoucherDAO Instance
+        {
+            get { 
+                if (instance == null)
+                {
+                    instance = new VoucherDAO();
+                }
+                return instance; 
+            }
+            private set => instance = value;
+        }
+        private VoucherDAO() { }
+        public List<Voucher> getVoucher()
+        {
+            List<Voucher> list = new List<Voucher>();
+            string query = @"Select * from VOUCHER";
+            DataTable data = DataProvider.Instance.ExcuteQuery(query);
+            foreach (DataRow dr in data.Rows)
+            {
+                list.Add(new Voucher(dr));
+            }
+            return list;
+        }
+        public List<Voucher> getVoucher(string idVoucher)
+        {
+            List<Voucher> list = new List<Voucher>();
+            string query = @"Select * from VOUCHER where IDVOUCHER = '" + idVoucher + "'";
+            DataTable data = DataProvider.Instance.ExcuteQuery(query);
+            foreach (DataRow dr in data.Rows)
+            {
+                list.Add(new Voucher(dr));
+            }
+            return list;
+        }
     }
 }
